d5caf7ee82ee4d916b267a97b1948b7d4639dd6a
Esther-Anyona <jkemuntoe@gmail.com>
2021-11-15 23:35:54 +0300

    Add styles for the page, form and table data well displayed

--- scripts.js
+++ scripts.js
@@ -4,8 +4,22 @@ const femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let day= document.getElementById("day");
 let month= document.getElementById("month");
 let year= document.getElementById("year");
-// let d=Math.floor(((year.slice(0,2)/4) - 2*(year.slice(0,2))-1)+ (5*(year.slice(2,4))/4)+ ((26*(month+1)/10)) + day) %7;
-var formData= document.getElementById('form');
+let male= document.getElementsByName('male');
+let female= document.getElementsByName("female")
+
+  function dayOfTheWeek () {
+    CC= parseInt(year.substring(0,2));
+    YY= parseInt(year.substring(0,2));
+    MM= parseInt(month);
+    DD= parseInt(date);
+    d= (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+  }
+
+
+
+  var formData= document.getElementById('form');
 formData.addEventListener('submit', function (){
   var myDay= day.value;
   var myMonth= month.value;
@@ -20,16 +34,11 @@ formData.addEventListener('submit', function (){
   else{
     return true;
   }
+  let results= d;
   });
 
-  function dayOfTheWeek () {
-    CC= parseInt(year.substring(0,2));
-    YY= parseInt(year.substring(0,2));
-    MM= parseInt(month);
-    DD= parseInt(date);
-    d= (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
-    console.log(d);
-    return (Math.floor(d));
-  }
 
-
+formData.onsubmit ()
+if (dayOfWeek[0] && maleNames[0] ) {
+  alert("Kwasi");
+}
