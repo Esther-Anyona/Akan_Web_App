b48447b7c1ed56dbf4e7bad7820665f58777c8dc
Esther-Anyona <jkemuntoe@gmail.com>
2021-11-15 21:41:05 +0300

    Add formula for calculating the day of the week

--- scripts.js
+++ scripts.js
@@ -22,3 +22,14 @@ formData.addEventListener('submit', function (){
   }
   });
 
+  function dayOfTheWeek () {
+    CC= parseInt(year.substring(0,2));
+    YY= parseInt(year.substring(0,2));
+    MM= parseInt(month);
+    DD= parseInt(date);
+    d= (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+  }
+
+
