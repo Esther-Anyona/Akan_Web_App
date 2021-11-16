0cbf5298637746c75df214b07e2c85daab170360
Esther-Anyona <jkemuntoe@gmail.com>
2021-11-16 21:50:13 +0300

    Validate form field entries

--- scripts.js
+++ scripts.js
@@ -13,7 +13,18 @@ function akanName(){
   var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var genderMale= document.getElementById("male");
   var genderFemale= document.getElementById("female");
-
+  if (year.value==""|| month.value=="" || day.value==""){
+    alert("Empty entry! confirm that all entries are filled before submitting");
+    return false;
+  }
+  if ((month.value<1) || (month.value>12)){
+    alert("Enter a valid value for month");
+    return false;
+  }
+  if ((day.value<1) || (day.value>31)){
+    alert("Enter a valid value for day");
+    return false;
+  }
   if (genderMale.checked){
     alert("Hey!" + " " + "You are a male and your Akan name is" + " " + maleNames[calculateDay.getDay()] + " " + ": You were born on a" + " " + daysOfWeek[calculateDay.getDay()]);
     year.value=""; month.value=""; day.value="";
